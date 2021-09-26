dd9280938805e50dad64a929804b645d58f67ed2
Rajnish Patel <rajnish17100042@gmail.com>
2021-09-26 23:53:10 +0530

    added feature to clear the history after pressing the clear button

--- js/script.js
+++ js/script.js
@@ -1,47 +1,40 @@
-let history = ''; //to store the history;
+let history = ""; //to store the history;
 
 let clearScreen = () => {
-
-    document.getElementById('resultScreen').value = '';
-
-
-}
+  document.getElementById("resultScreen").value = "";
+  document.getElementById("showHistory").value = "";
+};
 
 let display = (value) => {
+  // console.log(history);
+  // console.log(`data type of variable value is: ${typeof value}`);
+  // value = document.getElementById('resultScreen').value;
+  // console.log(`data type of variable value is: ${typeof value}`); ///string
 
+  let ops = ["+", "-", "/", "*", "."];
 
-    // console.log(history);
-    // console.log(`data type of variable value is: ${typeof value}`);
-    // value = document.getElementById('resultScreen').value;
-    // console.log(`data type of variable value is: ${typeof value}`); ///string
+  // check for the correct expression like  5+2 ...not 5++2
 
-    let ops = ['+', '-', '/', '*', '.'];
+  if (
+    (ops.includes(value) && history === "") ||
+    (ops.includes(value) && ops.includes(history.slice(-1)))
+  ) {
+    // alert('yes');
+    alert("not a valid expression");
+    return;
+  }
 
-    // check for the correct expression like  5+2 ...not 5++2
+  //adding value to the history
+  history += value;
+  // alert(history);
 
+  document.getElementById("resultScreen").value += value; //string + number=string
 
-    if (
-        ops.includes(value) && history === '' ||
-        ops.includes(value) && ops.includes(history.slice(-1))
-
-    ) {
-        // alert('yes');
-        alert('not a valid expression');
-        return;
-    }
-
-    //adding value to the history
-    history += value;
-    // alert(history);
-
-    document.getElementById('resultScreen').value += value;  //string + number=string
-
-    document.getElementById('showHistory').value += value;
-}
+  document.getElementById("showHistory").value += value;
+};
 
 let calculate = () => {
-    let result = document.getElementById('resultScreen').value;
-    document.getElementById('resultScreen').value = eval(result);
-    document.getElementById('showHistory').value += " = " + eval(result);
-
-}
+  let result = document.getElementById("resultScreen").value;
+  document.getElementById("resultScreen").value = eval(result);
+  document.getElementById("showHistory").value += " = " + eval(result);
+};
